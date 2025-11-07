660e134e4b21ac042fcd0a92abed30d5c6384fc0
rendyfutsuy <rendy.anggara@roketin.com>
2025-11-07 20:51:04 +0700

    fix deprecated test

--- Modules/Time/Tests/Unit/DurationTest.php
+++ Modules/Time/Tests/Unit/DurationTest.php
@@ -26,7 +26,7 @@ class DurationTest extends TestCase
 
         $time = new Duration($x, $y);
 
-        $this->assertEquals('05:30:30', $time->inTime());
+        $this->assertSame('05:30:30', $time->inTime());
     }
 
     /** @test */
@@ -37,28 +37,28 @@ class DurationTest extends TestCase
 
         $time = new Duration($x, $y);
 
-        $this->assertEquals(19830, $time->inSecond());
+        $this->assertSame(19830, $time->inSecond());
     }
 
     /** @test */
     public function assert_diff_in_minutes_equals_start_time_and_end_time_duration()
     {
         $x = Carbon::parse('2021-09-09 10:00:00');
-        $y = Carbon::parse('2021-09-09 15:30:30');
+        $y = Carbon::parse('2021-09-09 15:30:00');
 
         $time = new Duration($x, $y);
 
-        $this->assertEquals(330, $time->inMinute());
+        $this->assertSame(330, $time->inMinute());
     }
 
     /** @test */
     public function assert_diff_in_hours_equals_start_time_and_end_time_duration()
     {
         $x = Carbon::parse('2021-09-09 10:00:00');
-        $y = Carbon::parse('2021-09-09 15:30:30');
+        $y = Carbon::parse('2021-09-09 15:00:00');
 
         $time = new Duration($x, $y);
 
-        $this->assertEquals('5', $time->inHour());
+        $this->assertSame(5, $time->inHour());
     }
 }
